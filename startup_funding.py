089f9eb34182840570081adee080f07192e884b0
yashthakur-01 <yashthakurr001@gmail.com>
2025-08-08 13:08:52 +0530

    further work

--- startup_funding.py
+++ startup_funding.py
@@ -1,14 +1,44 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
 
+st.set_page_config(layout='wide',page_title ="Indian startup analysis")
+                   
+                   
+                   
 def investor_details(option):
+    st.header(option)
+    st.markdown("<br><br>", unsafe_allow_html=True)
+    # load most recent investments.
+    st.subheader('Most recent investments.')
     temp = df[df["investor"].str.contains(option,na=False)]
-    return temp.sort_values('date',ascending=False)
-
+    st.dataframe(temp.sort_values('date',ascending=False).head()[['date','startup','vertical','city','round','amount']])
+    st.markdown("<br><br>", unsafe_allow_html=True)
+    
+    
+    # biggest investment in each startup
+    st.subheader('Biggest Investment in each Company')
+    idx = df[df['investor'].str.contains(option, na=False)] \
+    .groupby('startup')['amount'].idxmax()
+    print (idx)
+    temp2 = df.loc[idx][['startup','amount']]
+    print(temp2)
+    st.dataframe(temp2)
+    st.markdown("<br><br>", unsafe_allow_html=True)
+    
+    
+    # biggest investments till now
+    st.subheader('Biggest investments till now')
+    temp3 = df[df['investor'].str.contains(option,na=False)].groupby('startup',as_index=False)\
+    ['amount'].sum().sort_values('amount',ascending=False).reset_index(drop=True).head()
+    fig,ax=plt.subplots()
+    ax.bar(temp3['startup'],temp3['amount'])
+    st.pyplot(fig)
+    
 
 # Download latest version
-path = 'C:/Users/yasht/OneDrive/Desktop/fast api/streamlit/startup_cleaned.csv'
+path = 'C:/Users/yasht/OneDrive/Desktop/fast api/streamlit/startup_cleaned (1).csv'
 
 
 df = pd.read_csv(path)
@@ -28,10 +58,9 @@ elif option == 'Investor':
     st.title('Investor Anlysis')
     btn2 = st.sidebar.button('Find Investor Details')
     if(btn2):
-        col1,col2,col3 = st.columns([1,2,1])
+        col1,col2,col3 = st.columns([1,6,1])
         with col2:
-            st.header(option)
-            st.markdown("<br><br>", unsafe_allow_html=True)
-            st.dataframe(investor_details(option))
+            investor_details(option)
+            
             
             
